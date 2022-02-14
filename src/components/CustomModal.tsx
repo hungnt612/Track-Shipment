18f8e84c75fd4d642084613057f8048140168460
hungnt612 <hungnt612@gmail.com>
2022-02-14 14:52:14 +0700

    init

--- src/components/CustomModal.tsx
+++ src/components/CustomModal.tsx
@@ -28,6 +28,69 @@ type CustomProperties = {
 
 const CustomModal: React.FC<CustomProperties> = ({item, visible, type}) => {
   // var _ = require('lodash');
+  const [text, onChangeText] = React.useState('');
+  const renderBtn = () => {
+    return (
+      <View
+        style={{
+          width: '80%',
+          height: 80,
+          backgroundColor: COLORS.lightOrange,
+          marginVertical: 26,
+          borderRadius: 20,
+          justifyContent: 'center',
+          alignSelf: 'center',
+        }}>
+        <TouchableOpacity
+          style={{
+            width: '96%',
+            height: '86%',
+            borderRadius: 18,
+            backgroundColor: COLORS.black2,
+            flexDirection: 'row',
+            alignSelf: 'center',
+          }}
+          onPress={() => {
+            console.log(text);
+          }}>
+          <View
+            style={{
+              flex: 2,
+              // backgroundColor: 'red',
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}>
+            <Text style={{color: COLORS.lightOrange, ...FONTS.h3}}>
+              Find my package
+            </Text>
+          </View>
+          <View
+            style={{
+              flex: 1,
+              // backgroundColor: 'blue'
+              justifyContent: 'center',
+              alignItems: 'flex-end',
+              marginRight: 10,
+            }}>
+            <View
+              style={{
+                width: 48,
+                height: 48,
+                backgroundColor: COLORS.lightOrange,
+                borderRadius: 50,
+                justifyContent: 'center',
+                alignItems: 'center',
+              }}>
+              <Image
+                source={icons.deliveryTruck}
+                style={{width: 25, height: 25}}
+              />
+            </View>
+          </View>
+        </TouchableOpacity>
+      </View>
+    );
+  };
 
   return (
     <Modal animationType="slide" transparent={true} visible={visible}>
@@ -54,14 +117,40 @@ const CustomModal: React.FC<CustomProperties> = ({item, visible, type}) => {
           activeOpacity={1}>
           <View
             style={{
+              justifyContent: 'center',
+              alignItems: 'center',
               alignSelf: 'center',
               marginTop: '75%',
               width: '85%',
               height: '30%',
               // backgroundColor: 'rgba(0,0,0,0.5)',
               backgroundColor: COLORS.black2,
+              borderRadius: 18,
             }}>
-            <Text>hihi</Text>
+            {/* <Text style={{color: 'white'}}>hihi</Text> */}
+            <View
+              style={{
+                width: '80%',
+                height: '24%',
+                backgroundColor: 'blue',
+                borderRadius: 18,
+                justifyContent: 'center',
+                alignItems: 'center',
+              }}>
+              <TextInput
+                onChangeText={onChangeText}
+                value={text}
+                style={{
+                  width: '96%',
+                  height: '86%',
+                  borderRadius: 18,
+                  backgroundColor: 'red',
+                }}
+                placeholder="Input your code"
+                textAlign={'center'}
+              />
+            </View>
+            {renderBtn()}
           </View>
         </TouchableOpacity>
       </BlurView>
